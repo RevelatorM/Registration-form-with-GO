df6032d6951f757dae9dc323378e48cc667d6af5
RevelatorM <152555320+RevelatorM@users.noreply.github.com>
2026-01-05 21:54:58 +0100

    Update main.go

--- GO/main.go
+++ GO/main.go
@@ -1,26 +1,37 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 )
 
 // ========================
 type SiteData struct {
-	email    string
-	password string
+	email         string
+	password      string
+	Success       bool
+	StorageAccess string
 }
 
 // ========================
+var (
+	tmpl = template.Must(template.ParseFiles("index.html")) //relation between GO and HTML
+)
+
+// ========================
 func handler(w http.ResponseWriter, req *http.Request) { //separeted handler function
-	data := SiteData {
-		email: r.FormValue("email")
-		password: r.FormValue("password")
+	data := SiteData{
+		email:    req.FormValue("email"),
+		password: req.FormValue("password"),
 	}
+	data.Success = true
+	data.StorageAccess = "Registrtion Completed!"
+	tmpl.Execute(w, data)
 }
 
 // ========================
 func main() {
-	http.HandlerFunc("/", handler) // "/" - root
+	http.HandleFunc("/", handler) // "/" - root
 	log.Fatal(http.ListenAndServe(":80", nil))
 }
