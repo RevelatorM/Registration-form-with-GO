943af819de1be3695ab8c87ff258403129cd84a0
RevelatorM <152555320+RevelatorM@users.noreply.github.com>
2026-01-05 21:38:15 +0100

    Update main.go

--- GO/main.go
+++ GO/main.go
@@ -1,1 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+// ========================
+type SiteData struct {
+	email    string
+	password string
+}
+
+// ========================
+func handler(w http.ResponseWriter, req *http.Request) { //separeted handler function
+
+}
+
+// ========================
+func main() {
+	http.HandlerFunc("/", handler) // "/" - root
+	log.Fatal(http.ListenAndServe(":80", nil))
+}
 
